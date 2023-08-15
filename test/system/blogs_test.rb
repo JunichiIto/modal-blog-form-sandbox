9d897830c0d72f9de8707568378a9a374c73bf66
Junichi Ito <jit@sonicgarden.jp>
2023-08-15 20:55:55 +0900

    Add system tests

--- test/system/blogs_test.rb
+++ test/system/blogs_test.rb
@@ -1,43 +1,73 @@
 require "application_system_test_case"
 
 class BlogsTest < ApplicationSystemTestCase
-  setup do
-    @blog = blogs(:one)
+  def fill_in_blog_content(text)
+    find("trix-editor").set(text)
   end
 
-  test "visiting the index" do
+  def assert_blogs(expected_blogs)
+    actual_blogs = all('#blogs > div')
+    assert_equal expected_blogs.size, actual_blogs.size
+    expected_blogs.each_with_index do |expected_blog, n|
+      within actual_blogs[n] do
+        title = find('h2').text
+        content = find('.trix-content').text
+        assert_equal expected_blog[:title], title
+        assert_equal expected_blog[:content], content
+      end
+    end
+  end
+
+  test "CRUD" do
     visit blogs_url
     assert_selector "h1", text: "Blogs"
-  end
+    assert_blogs([
+                   { title: 'My first blog', content: 'Hi there!' },
+                 ])
 
-  test "should create blog" do
-    visit blogs_url
-    click_on "New blog"
+    # Create
+    click_link 'New blog'
+    assert_selector 'h1', text: 'New blog'
+    # validation error
+    fill_in 'Title', with: ' '
+    click_button 'Create Blog'
+    assert_text "can't be blank"
 
-    fill_in "Content", with: @blog.content
-    fill_in "Title", with: @blog.title
-    click_on "Create Blog"
+    fill_in 'Title', with: 'Hello'
+    fill_in_blog_content 'World!'
+    click_button 'Create Blog'
+    assert_no_selector 'h1', text: 'New blog'
+    assert_text 'Blog was successfully created.'
+    assert_blogs([
+                   { title: 'Hello', content: 'World!' },
+                   { title: 'My first blog', content: 'Hi there!' },
+                 ])
 
-    assert_text "Blog was successfully created"
-    click_on "Back"
-  end
+    # Update
+    click_link 'Hello'
+    assert_selector 'h1', text: 'Editing blog'
+    # validation error
+    fill_in 'Title', with: ' '
+    click_button 'Update Blog'
+    assert_text "can't be blank"
 
-  test "should update Blog" do
-    visit blog_url(@blog)
-    click_on "Edit this blog", match: :first
+    fill_in 'Title', with: 'My Turbo'
+    fill_in_blog_content 'Turbo is fun!'
+    click_button 'Update Blog'
+    assert_no_selector 'h1', text: 'Editing blog'
+    assert_text 'Blog was successfully updated.'
+    assert_blogs([
+                   { title: 'My Turbo', content: 'Turbo is fun!' },
+                   { title: 'My first blog', content: 'Hi there!' },
+                 ])
 
-    fill_in "Content", with: @blog.content
-    fill_in "Title", with: @blog.title
-    click_on "Update Blog"
-
-    assert_text "Blog was successfully updated"
-    click_on "Back"
-  end
-
-  test "should destroy Blog" do
-    visit blog_url(@blog)
-    click_on "Destroy this blog", match: :first
-
-    assert_text "Blog was successfully destroyed"
+    # Delete
+    click_link 'My Turbo'
+    assert_selector 'h1', text: 'Editing blog'
+    find('.delete-button').click
+    assert_text 'Blog was successfully destroyed.'
+    assert_blogs([
+                   { title: 'My first blog', content: 'Hi there!' },
+                 ])
   end
 end
